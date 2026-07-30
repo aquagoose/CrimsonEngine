4c87775934bc82d8c01931132a7567f8a827f4d6
Aqua <aqua@aquagoose.com>
2026-07-30 13:08:35 +0100

    begin renderer

---
+++ src/Crimson.Graphics/Renderer.cs
@@ -0,0 +1,16 @@
+﻿using piko.SDL3;
+
+namespace Crimson.Graphics;
+
+public static class Renderer
+{
+    public static void Init(SDL.Window window)
+    {
+
+    }
+
+    public static void Free()
+    {
+
+    }
+}
