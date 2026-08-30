0e2d5563cbadec416135f29d4c9c3b34a7fcc4c6
Aqua <aqua@aquagoose.com>
2026-08-30 21:13:50 +0100

    start renderer

---
+++ src/Crimson.Graphics/Renderer.cs
@@ -0,0 +1,46 @@
+﻿using Crimson.Core;
+using piko.SDL3;
+
+namespace Crimson.Graphics;
+
+/// <summary>
+/// Performs 2D and 3D rendering to draw objects to the display.
+/// </summary>
+public class Renderer : IDisposable
+{
+    private readonly SDL.Window _window;
+
+    internal readonly SDL.GPUDevice Device;
+
+    public Renderer(SDL.Window window)
+    {
+        _window = window;
+
+        uint props = SDL.CreateProperties();
+        // always enable vulkan as a fallback and for linux
+        SDL.SetBooleanProperty(props, SDL.Prop.GpuDeviceCreateShadersSpirvBoolean, true);
+
+        // enable d3d12 on windows
+        if (OperatingSystem.IsWindows())
+            SDL.SetBooleanProperty(props, SDL.Prop.GpuDeviceCreateShadersDxilBoolean, true);
+
+        // enable metal on macos
+        if (OperatingSystem.IsMacOS())
+            SDL.SetBooleanProperty(props, SDL.Prop.GpuDeviceCreateShadersMslBoolean, true);
+
+#if DEBUG
+        SDL.SetBooleanProperty(props, SDL.Prop.GpuDeviceCreateDebugmodeBoolean, true);
+        SDL.SetBooleanProperty(props, SDL.Prop.GpuDeviceCreateVerboseBoolean, true);
+#endif
+
+        Logger.Trace("Creating device.");
+        Device = SDL.CreateGPUDeviceWithProperties(props);
+    }
+
+    public void Dispose()
+    {
+        SDL.WaitForGPUIdle(Device);
+
+        SDL.DestroyGPUDevice(Device);
+    }
+}
