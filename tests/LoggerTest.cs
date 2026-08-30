4d6492c06113757dd49873b0c2957a63b2599bb9
Aqua <aqua@aquagoose.com>
2026-08-30 16:07:45 +0100

    logger test

---
+++ tests/LoggerTest.cs
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env dotnet
+#:project ../src/Crimson.Core/Crimson.Core.csproj
+
+using Crimson.Core;
+
+Logger.Trace("Trace message");
+Logger.Debug("Debug message");
+Logger.Info("Info message");
+Logger.Warn("Warning message");
+Logger.Error("Error message");
+Logger.Fatal("Fatal message");
