d2f116f0de1ab63a88cbb133b28dd63ae4b151cb
Aqua <aqua@aquagoose.com>
2026-09-16 18:42:21 +0100

    logger

---
+++ src/Crimson.Core/Logger.cs
@@ -0,0 +1,194 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Crimson.Core;
+
+/// <summary>
+/// Log useful messages for debugging.
+/// </summary>
+public static class Logger
+{
+    /// <summary>
+    /// Invoked when a new message is logged.
+    /// </summary>
+    public static event OnMessageLogged MessageLogged;
+
+    private static StringBuilder _sb;
+
+    /// <summary>
+    /// Enable/disable logging to the console. If compiling with DEBUG, this is enabled by default.
+    /// </summary>
+    public static bool LogToConsole
+    {
+        get => field;
+        set
+        {
+            // ensure we don't subscribe/unsubscribe to the event more than once
+            if (field == value)
+                return;
+
+            field = value;
+
+            if (field)
+                MessageLogged += WriteMessageToConsole;
+            else
+                MessageLogged -= WriteMessageToConsole;
+        }
+    }
+
+    static Logger()
+    {
+        _sb = new StringBuilder();
+        MessageLogged = delegate { };
+#if DEBUG
+        LogToConsole = true;
+#endif
+    }
+
+    /// <summary>
+    /// Log a message.
+    /// </summary>
+    /// <param name="severity">The message's <see cref="Severity"/>.</param>
+    /// <param name="message">The message to log.</param>
+    /// <param name="line">The calling line number.</param>
+    /// <param name="file">The calling file path.</param>
+    public static void Log(Severity severity, string message, [CallerLineNumber] int line = 0, [CallerFilePath] string file = "")
+    {
+        _sb.Clear();
+
+        string now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff ");
+        string fileName = Path.GetFileName(file);
+
+        _sb.Append(now);
+
+        _sb.Append(severity switch
+        {
+            Severity.Trace   => "[Trace] ",
+            Severity.Debug   => "[Debug] ",
+            Severity.Info    => "[Info]  ",
+            Severity.Warning => "[Warn]  ",
+            Severity.Error   => "[Error] ",
+            Severity.Fatal   => "[FATAL] ",
+            _ => throw new ArgumentOutOfRangeException(nameof(severity), severity, null)
+        });
+
+        _sb.Append('(');
+        _sb.Append(fileName);
+        _sb.Append(':');
+        _sb.Append(line);
+        _sb.Append(')');
+        _sb.Append(' ');
+
+        _sb.Append(message);
+
+        MessageLogged(_sb.ToString(), severity);
+    }
+
+    /// <summary>
+    /// Log a <see cref="Severity.Trace"/> message.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    /// <param name="line">The calling line number.</param>
+    /// <param name="file">The calling file path.</param>
+    public static void Trace(string message, [CallerLineNumber] int line = 0, [CallerFilePath] string file = "")
+        => Log(Severity.Trace, message, line, file);
+
+    /// <summary>
+    /// Log a <see cref="Severity.Debug"/> message.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    /// <param name="line">The calling line number.</param>
+    /// <param name="file">The calling file path.</param>
+    public static void Debug(string message, [CallerLineNumber] int line = 0, [CallerFilePath] string file = "")
+        => Log(Severity.Debug, message, line, file);
+
+    /// <summary>
+    /// Log a <see cref="Severity.Info"/> message.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    /// <param name="line">The calling line number.</param>
+    /// <param name="file">The calling file path.</param>
+    public static void Info(string message, [CallerLineNumber] int line = 0, [CallerFilePath] string file = "")
+        => Log(Severity.Info, message, line, file);
+
+    /// <summary>
+    /// Log a <see cref="Severity.Warning"/> message.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    /// <param name="line">The calling line number.</param>
+    /// <param name="file">The calling file path.</param>
+    public static void Warn(string message, [CallerLineNumber] int line = 0, [CallerFilePath] string file = "")
+        => Log(Severity.Warning, message, line, file);
+
+    /// <summary>
+    /// Log a <see cref="Severity.Error"/> message.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    /// <param name="line">The calling line number.</param>
+    /// <param name="file">The calling file path.</param>
+    public static void Error(string message, [CallerLineNumber] int line = 0, [CallerFilePath] string file = "")
+        => Log(Severity.Error, message, line, file);
+
+    /// <summary>
+    /// Log a <see cref="Severity.Fatal"/> message.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    /// <param name="line">The calling line number.</param>
+    /// <param name="file">The calling file path.</param>
+    public static void Fatal(string message, [CallerLineNumber] int line = 0, [CallerFilePath] string file = "")
+        => Log(Severity.Fatal, message, line, file);
+
+    private static void WriteMessageToConsole(string message, Severity severity)
+    {
+        string color = severity switch
+        {
+            Severity.Trace   => "90", // gray
+            Severity.Debug   => "0",  // default terminal color
+            Severity.Info    => "96", // cyan
+            Severity.Warning => "93", // yellow
+            Severity.Error   => "91", // red
+            Severity.Fatal   => "31", // dark red
+            _ => throw new ArgumentOutOfRangeException(nameof(severity), severity, null)
+        };
+
+        Console.WriteLine($"\e[{color}m{message}\e[0m");
+    }
+
+    /// <summary>
+    /// Defines the various severities that a log message can be.
+    /// </summary>
+    public enum Severity
+    {
+        /// <summary>
+        /// Verbose messages, logging exact codepaths and trace debug info.
+        /// </summary>
+        Trace,
+
+        /// <summary>
+        /// Debug messages that are primarily useful for debugging.
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        /// Important information.
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// Something isn't right, but it was handled.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// Something went wrong, but it was handled and program execution can continue.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Something went very wrong and program execution cannot continue.
+        /// </summary>
+        Fatal
+    }
+
+    public delegate void OnMessageLogged(string message, Severity severity);
+}
---
+++ tests/LoggerTest.cs
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env dotnet
+#:project ../src/Crimson.Core/Crimson.Core.csproj
+
+using System.Diagnostics;
+using Crimson.Core;
+
+#if DEBUG
+Debug.Assert(Logger.LogToConsole == true);
+#else
+Logger.LogToConsole = true
+#endif
+
+Logger.Trace("Trace message");
+Logger.Debug("Debug message");
+Logger.Info("Info message");
+Logger.Warn("Warning message");
+Logger.Error("Error message");
+Logger.Fatal("Fatal message");
