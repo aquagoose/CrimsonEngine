18d3dbffa793b04d9f29dba77c504cd6a1308580
Aqua <aqua@aquagoose.com>
2026-09-25 22:17:21 +0100

    logger

--- Engine/include/Crimson/Core/Logger.h
+++ Engine/include/Crimson/Core/Logger.h
@@ -1,6 +1,67 @@
 #pragma once
 
-namespace cge
-{
+#include <string>
+#include <source_location>
+#include <format>
+#include <stdexcept>
 
+#define CGE_LOG(severity, ...) {\
+    auto message = std::format(__VA_ARGS__);\
+    cge::Logger::Log(cge::Logger::Severity::severity, message);\
+    if (cge::Logger::Severity::severity == cge::Logger::Severity::Fatal)\
+        throw std::runtime_error(message);\
+}
+
+#define CGE_TRACE(...) CGE_LOG(Trace, __VA_ARGS__)
+#define CGE_DEBUG(...) CGE_LOG(Debug, __VA_ARGS__)
+#define CGE_INFO(...) CGE_LOG(Info, __VA_ARGS__)
+#define CGE_WARN(...) CGE_LOG(Warning, __VA_ARGS__)
+#define CGE_ERROR(...) CGE_LOG(Error, __VA_ARGS__)
+#define CGE_FATAL(...) CGE_LOG(Fatal, __VA_ARGS__)
+
+namespace cge::Logger
+{
+    /**
+     * Defines the severity of a log message.
+     */
+    enum class Severity
+    {
+        /**
+         * Verbose messages, logging exact code paths.
+         */
+        Trace,
+
+        /**
+         * Debug information.
+         */
+        Debug,
+
+        /**
+         * Useful information.
+         */
+        Info,
+
+        /**
+         * Something isn't right, but it was handled.
+         */
+        Warning,
+
+        /**
+         * Something bad happened, but program execution can continue.
+         */
+        Error,
+
+        /**
+         * Something VERY bad happened, and program execution cannot continue.
+         */
+        Fatal
+    };
+
+    /**
+     * Log a message.
+     * @param severity The message severity.
+     * @param message The message to log.
+     * @param location The location where this function was called.
+     */
+    void Log(Severity severity, const std::string_view& message, std::source_location location = std::source_location::current());
 }
--- Engine/src/Core/Logger.cpp
+++ Engine/src/Core/Logger.cpp
@@ -1,6 +1,55 @@
 #include "Crimson/Core/Logger.h"
 
+#include <sstream>
+#include <chrono>
+#include <format>
+#include <filesystem>
+#include <iostream>
+
 namespace cge
 {
+    std::stringstream _ss;
 
+    void Logger::Log(Severity severity, const std::string_view& message, std::source_location location)
+    {
+        _ss.str("");
+        auto now = std::chrono::system_clock::now();
+        auto fileName = std::filesystem::path(location.file_name());
+
+        _ss << std::format("{:%F %T} ", std::chrono::round<std::chrono::milliseconds>(now));
+
+        std::string ansi;
+        switch (severity)
+        {
+            case Severity::Trace:
+                _ss << "[Trace] ";
+                ansi = "90"; // gray
+                break;
+            case Severity::Debug:
+                _ss << "[Debug] ";
+                ansi = "0"; // default terminal color
+                break;
+            case Severity::Info:
+                _ss << "[Info]  ";
+                ansi = "96"; // cyan
+                break;
+            case Severity::Warning:
+                _ss << "[Warn]  ";
+                ansi = "93"; // yellow
+                break;
+            case Severity::Error:
+                _ss << "[Error] ";
+                ansi = "91"; // red
+                break;
+            case Severity::Fatal:
+                _ss << "[FATAL] ";
+                ansi = "31"; // dark red
+                break;
+        }
+
+        _ss << '(' << fileName.filename() << ':' << location.line() << ") ";
+        _ss << message;
+
+        std::cout << "\e[" << ansi << "m" << message << "\e[0m" << std::endl;
+    }
 }
